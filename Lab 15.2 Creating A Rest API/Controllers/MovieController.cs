5dc118d9499c8f11f4ce4d3c47a11e957aaa32d1
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-09-22 19:03:10 -0400

    Add project files.

---
+++ Lab 15.2 Creating A Rest API/Controllers/MovieController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_15._2_Creating_A_Rest_API.Controllers
+{
+    [Route("Movie")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet("MovieList")]
+        public List<Movie> MovieList()
+        {
+            List<Movie> movie = new List<Movie>();
+            movie = Movie.ReadAll();
+            return movie;
+        }
+
+        [HttpGet("Category/{catid}")]
+        public List<Movie> CategorySearch(string catid)
+        {
+            List<Movie> movie = new List<Movie>();
+            movie = Movie.CategorySearch(catid);
+            return movie;
+        } 
+
+        [HttpGet("Random")]
+        public Movie RandomMovie()
+        {
+            Movie movie = new Movie();
+            movie = Movie.RandomMovie();
+            return movie;
+        }
+
+        [HttpGet("{cat}/Random")]
+        public Movie RandomCategory(string category)
+        {
+            Movie movie = new Movie();
+            movie = Movie.RandomCategory(category);
+            return movie;
+        }
+            
+    }
+
+   
+}
---
+++ Lab 15.2 Creating A Rest API/Movie.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace Lab_15._2_Creating_A_Rest_API
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public long id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        public static Random rand = new Random();
+        public static void Create(string title, string category)
+        {
+            IDbConnection db = new SqlConnection("Server=HN78Q13\\SQLEXPRESS;Database=MovieAPI;user id=testuser;password=abc123");
+            Movie movie = new Movie()
+            {
+                Title = title,
+                Category = category
+            };
+            db.Insert(movie);
+        }
+
+        public static List<Movie> ReadAll()
+        {
+            IDbConnection db = new SqlConnection("Server=HN78Q13\\SQLEXPRESS;Database=MovieAPI;user id=testuser;password=abc123");
+            List<Movie> M = db.Query<Movie>($"select * from [Movie]").AsList<Movie>();
+            return M;
+        }
+
+        public static List<Movie> CategorySearch(string category)
+        {
+            IDbConnection db = new SqlConnection("Server=HN78Q13\\SQLEXPRESS;Database=MovieAPI;user id=testuser;password=abc123");
+            List<Movie> M = db.Query<Movie>($"select * from [Movie] where Category = {category}").AsList<Movie>();
+            return M;
+        }
+
+        public static List<Movie> TitleSearch(string title)
+        {
+            IDbConnection db = new SqlConnection("Server=HN78Q13\\SQLEXPRESS;Database=MovieAPI;user id=testuser;password=abc123");
+            List<Movie> M = db.Query<Movie>($"select * from [Movie] where Title = {title}").AsList<Movie>();
+            return M;
+        }
+
+        public static Movie RandomMovie()
+        {
+            IDbConnection db = new SqlConnection("Server=HN78Q13\\SQLEXPRESS;Database=MovieAPI;user id=testuser;password=abc123");
+            List<Movie> M = db.Query<Movie>($"select * from [Movie]").AsList<Movie>();
+            int result = rand.Next(0, M.Count);
+            return M[result];
+        }
+
+        public static Movie RandomCategory(string category)
+        {
+            List<Movie> M = new List<Movie>();
+            int result = rand.Next(0, M.Count);
+            return M[result];
+        }
+    }   
+}
